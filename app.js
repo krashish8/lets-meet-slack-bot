da6220d61d494dbf3d1a52e2ff01db414f66bdca
Ashish Kumar <ashishkr23438@gmail.com>
2020-02-15 02:01:29 +0530

    Response URL

--- app.js
+++ app.js
@@ -56,12 +56,13 @@ function check_registered(email) {
 
 app.post('/propose-meet', function(request, result, next) {
 	user_id = request.body.user_id;
+	response_url = request.body.response_url
 	get_email_from_user_id(user_id).then(email => {
 		check_registered(email).then(token => {
-			return result.status(200).json({'text': 'You are registered on Let\'s meet with token ' + token});
+			return response_url.status(200).json({'text': 'You are registered on Let\'s meet with token ' + token});
 		},
 		error => {
-			return result.status(200).json({'text': 'You are not registered on Let\'s meet'});
+			return response_url.status(200).json({'text': 'You are not registered on Let\'s meet'});
 		})
 	})
 
